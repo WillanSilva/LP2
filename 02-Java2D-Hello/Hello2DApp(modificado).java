188e8fdb37f9f0ed9e8ab84c076f7bcf52a90b41
Willan Silva <48417209+WillanSilva@users.noreply.github.com>
2021-08-09 19:32:55 -0300

    Update Hello2DApp(modificado).java

--- 02-Java2D-Hello/Hello2DApp(modificado).java
+++ 02-Java2D-Hello/Hello2DApp(modificado).java
@@ -19,7 +19,7 @@ class Hello2DFrame extends JFrame {
         );
         this.setTitle("Java2D - Hello World!");
         this.setSize(350, 350);
-	this.getContentPane().setBackground(Color.black); /*get contentPane() é uma método de Jframe para "pegar" a janela para após colocar a cor black*/
+	this.getContentPane().setBackground(Color.black); /*alteração 3: backgroud para black, get contentPane() é uma método de Jframe para "pegar" a janela para após, colocar a cor black*/
         this.setVisible(true);
 	
     }
@@ -27,11 +27,13 @@ class Hello2DFrame extends JFrame {
     public void paint (Graphics g) {
         super.paint(g);
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setPaint(Color.green); /*coloquei para verde todos os desenhos*/
+        g2d.setPaint(Color.green); /*alteração 1: g2d.setPaint(Color.blue) --> g2d.setPaint(Color.green), coloquei para verde todos os desenhos*/
         int w = getWidth();
         int h = getHeight();
         g2d.drawLine(0,0, w,h);
         g2d.drawLine(0,h, w,0);
-        g2d.draw3DRect(150,150,50,60,false); /*adicionei retangulo, draw3drect é um método de Graphics2D*/
+	String nome="Antonio Willan";
+	g2d.drawString(nome,140,150);/*alteração 4: coloquei meu nome no meio para diferenciar*/
+        g2d.draw3DRect(150,150,50,60,false); /*alteração 2 -- adicionei retangulo, draw3drect é um método de Graphics2D*/
     }
 }
