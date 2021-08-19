f0e3a34c7b71ca09de7e86e88a6e3bc6e9bd4bcd
Willan Silva <48417209+WillanSilva@users.noreply.github.com>
2021-08-19 17:29:15 -0300

    Update Figura1.c

--- Módulo2/EXE1/Figura1.c
+++ Módulo2/EXE1/Figura1.c
@@ -1,1 +1,19 @@
-
+#include <stdio.h>
+typedef struct 
+{
+    int x;
+    int y;
+    int w; 
+    int h; /*width height and length*/
+    int l;
+    float raio; /*para a circurferencia*/
+    int r,g,b; /*Adicional para a a cor */
+}cilindro;
+int print(cilindro*s){
+    printf("Cilíndro de tamanho(%d,%d,%d)\nNa posição (%d,%d)\nCom Raio: %f\nE com as cores: r=%d, g=%d ,b=%d \n",
+    s->x,s->y,s->w,s->h,s->l,s->raio,s->r,s->g,s->b);
+}
+ int main(void){
+    cilindro eae={150,150,150,50,50,3.4,4,5,8};
+    print(&eae);
+ }
