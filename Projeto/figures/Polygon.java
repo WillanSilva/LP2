ed1249ee4ddba07e1b6ed31ad0091e8468728ec1
Willan Silva <48417209+WillanSilva@users.noreply.github.com>
2021-09-13 14:42:40 -0300

    Create Polygon.java

---
+++ Projeto/figures/Polygon.java
@@ -0,0 +1,18 @@
+package figures;
+import java.awt.*;
+public class Polygon extends Figure{
+    public Polygon(int[] xPoint,int[] yPoint, int nPoints,int r, int g, int b,int r2, int g2, int b2){
+        super(xPoint,yPoint,nPoints,r,g,b,r2,g2,b2);
+        
+    }
+    public void print () {
+        System.out.format("na posicao (%d,%d) com %d pontos (vértices).\n",this.x, this.y,this.p);
+    }
+    public void paint (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g; 
+        g2d.setColor(new Color(r,this.g,b));
+        g2d.fillPolygon(this.xPoint, this.yPoint,this.p);
+        g2d.setColor(new Color(r2,g2,b2));
+        g2d.drawPolyline(this.xPoint,this.yPoint, this.p);
+    }
+}
