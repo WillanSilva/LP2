56f385adfd677fa41439111d08b0bafede2da060
Willan Silva <48417209+WillanSilva@users.noreply.github.com>
2021-09-27 19:27:08 -0300

    Update Polygon.java

--- Projeto/figures/Polygon.java
+++ Projeto/figures/Polygon.java
@@ -1,9 +1,13 @@
 package figures;
 import java.awt.*;
 public class Polygon extends Figure{
+    protected int[] xPoint,yPoint; /* a classe Polygon é diferente da demais portanto criei 2 construtor */
+    protected int p;
     public Polygon(int[] xPoint,int[] yPoint, int nPoints,int r, int g, int b,int r2, int g2, int b2){
-        super(xPoint,yPoint,nPoints,r,g,b,r2,g2,b2);
-        
+        super(r,g,b,r2,g2,b2);
+        this.xPoint=xPoint;
+        this.yPoint=yPoint;
+        this.p=nPoints;
     }
     public void print () {
         System.out.format("na posicao (%d,%d) com %d pontos (vértices).\n",this.x, this.y,this.p);
