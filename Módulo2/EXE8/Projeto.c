cabecf7cdb1ca03cf89deb606f8d32c237d384c7
Willan Silva <48417209+WillanSilva@users.noreply.github.com>
2021-10-08 19:44:03 -0300

    Update Projeto.c

--- Módulo2/EXE8/Projeto.c
+++ Módulo2/EXE8/Projeto.c
@@ -7,17 +7,11 @@ typedef struct {
 
 struct Figure;
 typedef void (* Figure_Print) (struct Figure*);
-typedef int  (* Figure_Area)  (struct Figure*);
-
-typedef struct {
-    void (* print) (struct Figure*);
-    int  (* area)  (struct Figure*);
-} Figure_vtable;
 
 typedef struct Figure {
     int x, y;
     Color fg, bg;
-    Figure_vtable* vtable;
+    void (* print) (struct Figure*);
 } Figure;
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -29,24 +23,14 @@ typedef struct {
 
 void rect_print (Rect* this) {
     Figure* sup = (Figure*) this;
-    printf("Retangulo de tamanho (%d,%d) na posicao (%d,%d) e area %d.\n",
-           this->w, this->h, sup->x, sup->y, sup->vtable->area(sup));
+    printf("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
+           this->w, this->h, sup->x, sup->y);
 }
-
-int rect_area (Rect* this) {
-    Figure* sup = (Figure*) this;
-    return this->w * this->h;
-}
-
-Figure_vtable rect_vtable = {
-    (Figure_Print) rect_print,
-    (Figure_Area)  rect_area
-};
 
 Rect* rect_new (int x, int y, int w, int h) {
     Rect*   this  = malloc(sizeof(Rect));
     Figure* sup = (Figure*) this;
-    sup->vtable = &rect_vtable;
+    sup->print = (Figure_Print) rect_print;
     sup->x = x;
     sup->y = y;
     this->w = w;
@@ -60,26 +44,16 @@ typedef struct {
     int w, h;
 } Ellipse;
 
-void ellipse_print (Rect* this) {
+void Ellipse_print (Ellipse* this) {
     Figure* sup = (Figure*) this;
-    printf("Elipse de tamanho (%d,%d) na posicao (%d,%d) e area %d.\n",
-           this->w, this->h, sup->x, sup->y, sup->vtable->area(sup));
+    printf("Elipse de tamanho (%d,%d) na posicao (%d,%d).\n",
+           this->w, this->h, sup->x, sup->y);
 }
-
-int ellipse_area (Rect* this) {
-    Figure* sup = (Figure*) this;
-    return this->w * this->h;
-}
-
-Figure_vtable ellipse_vtable = {
-    (Figure_Print) ellipse_print,
-    (Figure_Area)  ellipse_area
-};
 
 Ellipse* ellipse_new (int x, int y, int w, int h) {
     Ellipse* this = malloc(sizeof(Ellipse));
     Figure* sup = (Figure*) this;
-    sup->vtable = &ellipse_vtable;
+    sup->print = (Figure_Print) Ellipse_print;
     sup->x = x;
     sup->y = y;
     this->w = w;
@@ -94,30 +68,36 @@ typedef struct {
     int nPoint;
 } Polygon;
 
-void Polygon_print (Rect* this) {
+void Polygon_print (Polygon* this) {
     Figure* sup = (Figure*) this;
-    printf("Polygon de tamanho (%d,%d) na posicao (%d,%d) e area %d.\n",
-           this->w, this->h, sup->x, sup->y, sup->vtable->area(sup));
+    printf("Polygon com as coordenadas x(%d,%d,%d,%d) e y(%d,%d,%d,%d) com %d vértices.\n",
+           sup->x, this->xPoint[1],this->xPoint[2],this->xPoint[3],sup->y,this->yPoint[1],this->yPoint[2],this->yPoint[3],this->nPoint);
+    printf("E com as cores de fundo:  rg(%d,%d,%d) Contorno: rg2(%d,%d,%d)\n",sup->bg.r,sup->bg.g,sup->bg.b,sup->fg.r,sup->fg.g,sup->fg.b);
 }
-int Polygon_area(Rect* this) {
+void polygon_perimetro(Polygon* this) {
     Figure* sup = (Figure*) this;
-    return this->w * this->h;
+    int peri=0;
+    for (int i=0; i<4; i++) {
+        peri+=(this->xPoint[i])-(this->yPoint[i]);
+        
+    }
+    if (peri<0){
+        peri=peri*(-1);
+    }
+    printf("Perimetro: %d",peri);
 }
-Figure_vtable Polygon_vtable = {
-    (Figure_Print) Polygon_print,
-    (Figure_Area)  Polygon_area
-};
-Polygon* Polygon_new (int xPoint[3],int yPoint[3], int nPoints,int r, int g, int b,int r2, int g2, int b2) {
+
+Polygon* Polygon_new (int xPoint[4],int yPoint[4], int nPoints,int r, int g, int b,int r2, int g2, int b2) {
     Polygon* this = malloc(sizeof(Polygon));
     Figure* sup = (Figure*) this;
-    sup->vtable = &Polygon_vtable;
+    sup->print = (Figure_Print) Polygon_print;
     sup->x= xPoint[0];
     sup->y = yPoint[0];
     Color rb={r,g,b};
     Color rb2={r2,g2,b2};
     sup->bg=rb;
     sup->fg=rb2;
-    for (int i = 0; i < 3; i++){
+    for (int i = 0; i < 4; i++){
         this->xPoint[i]=xPoint[i];
         this->yPoint[i]= yPoint[i];
     }
@@ -126,20 +106,20 @@ Polygon* Polygon_new (int xPoint[3],int yPoint[3], int nPoints,int r, int g, int
 }
 ///////////////////////////////////////////////////////////////////////////////
 void main (void) {
-    int xPoint[3]={89,54,77};
-    int yPoint[3]={45,80,90};
+    int xPoint[4]={4,5,92,3};
+    int yPoint[4]={50,30,65,77};
     Figure* figs[5] = {
         (Figure*) rect_new(10,10,100,100),
         (Figure*) ellipse_new(40,10,140,300),
         (Figure*) rect_new(10,10,100,100),
         (Figure*) ellipse_new(210,110,305,130),
-        (Figure*) Polygon_new(xPoint,yPoint,30,5,3,7,8,9,80)
+        (Figure*) Polygon_new(xPoint,yPoint,4,1,6,300,4,305,130)
     };
 
     ///
 
     for (int i=0; i<5; i++) {
-        figs[i]->vtable->print(figs[i]);
+        figs[i]->print(figs[i]);
     }
 
     ///
