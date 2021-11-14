0504d025c1086caa2b351638ed825c0c64ce0e62
Willan Silva <48417209+WillanSilva@users.noreply.github.com>
2021-11-14 19:09:55 -0300

    Update ArrayStack.java

--- Projeto/IStackable/ArrayStack.java
+++ Projeto/IStackable/ArrayStack.java
@@ -1,1 +1,25 @@
-public class 
+import java.util.*;
+import java.util.List;
+class ArrayStack implements IStackable {
+  List<Integer> l;
+  public ArrayStack() {
+    this.l = new ArrayList<Integer>();
+  }
+
+  public int size() {
+    return l.size();
+  } // quantos elementos
+
+  public void push(int v) {
+    l.add(v);
+  } // empilha inteiro
+
+  public int pop(){
+    int a=l.get(l.size()-1);
+    if (a!=0){
+      l.remove(l.size()-1);
+    }
+    return a;
+  } // quantos elementos
+
+}
