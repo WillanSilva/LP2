66089b84d1a3c69c2a7624a9a59f59f2a1a3241c
Willan Silva <48417209+WillanSilva@users.noreply.github.com>
2021-11-14 15:23:10 -0300

    Create Losang.java

---
+++ Projeto/figures/Losang.java
@@ -0,0 +1,20 @@
+package figures;
+import java.awt.*;
+public class Losang extends Figure{
+    public Losang (int x, int y, int w, int h,int r, int g, int b,int r2, int g2, int b2) {
+        super(x,x,w,h,r,g,b,r2,g2,b2);
+    }
+    public void print () {
+        System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
+    }
+    public void paint (Graphics g) {
+        int [] xPoint= {this.x, this.x, this.x};
+        int [] yPoint= {this.y, (this.y + this.h)/2,this.y + this.h};
+        Graphics2D g2d = (Graphics2D) g; 
+        g2d.setColor(new Color(r,this.g,b));
+        g2d.fillPolygon(xPoint, yPoint,4);
+        g2d.setColor(new Color(r2,g2,b2));
+        g2d.drawPolyline(xPoint,yPoint,4);
+    }
+}
