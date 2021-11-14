5ee3133341de87569786df641ce73d31cdf801a3
Willan Silva <48417209+WillanSilva@users.noreply.github.com>
2021-11-14 15:29:29 -0300

    Update IStackable.java

--- Projeto/IStackable/IStackable.java
+++ Projeto/IStackable/IStackable.java
@@ -1,2 +1,5 @@
-public class  IStackable {
+interface IStackable {
+    int  size ();       // quantos elementos
+    void push (int v);  // empilha inteiro
+    int  pop  ();       // desempilha inteiro
 }
