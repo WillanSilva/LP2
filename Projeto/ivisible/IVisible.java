966b02efe5fa3def24108abafade3b0ae8d00e58
Willan Silva <48417209+WillanSilva@users.noreply.github.com>
2021-11-17 17:29:29 -0300

    Update IVisible.java

--- Projeto/ivisible/IVisible.java
+++ Projeto/ivisible/IVisible.java
@@ -2,6 +2,6 @@ package ivisible;
 import java.awt.Graphics;
 public interface IVisible {
     public boolean clicked (int x, int y);
-    public void    paint   (Graphics g);
+    public void    paint   (Graphics g, boolean focused);
 }
 
