2bd8922f936155ea127688f7505416fd3daa1cbf
Willan Silva <48417209+WillanSilva@users.noreply.github.com>
2021-11-17 17:24:18 -0300

    Update and rename ivisible.java to IVisible.java

--- Projeto/ivisible/IVisible.java
+++ Projeto/ivisible/IVisible.java
@@ -1,6 +1,6 @@
 package ivisible;
 import java.awt.Graphics;
-public interface ivisible {
+public interface IVisible {
     public boolean clicked (int x, int y);
     public void    paint   (Graphics g);
 }
